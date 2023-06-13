d6a4d750114048ce2fffcb796d878f571a04a9a0
nadin869 <nady86.86@mail.ru>
2023-06-14 00:22:10 +0300

    создан пакет restclient

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+REQUIRES = [
+    'requests',
+    'structlog',
+    'curlify',
+    'allure-pytest'
+]
+setup(
+    name='restclient',
+    version='0.0.1',
+    packages=['restclient', 'restclient.restclient'],
+    url='https://github.com/NadezhdaDudnik/restclient.git',
+    license='MIT',
+    author='Nadezhda Dudnik',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='restclient with allure and logger'
+)
